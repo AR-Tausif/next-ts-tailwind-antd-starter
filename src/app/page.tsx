96fea81b35ce9135b86fe44383e5bd8e64067767
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-04-11 11:27:37 +0600

    feat: initial commit -setup Next.js with TypeScript, Tailwind Css, and Ant Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,44 @@
-import Image from "next/image";
+import { Github, Link2 } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+    <div className="w-full min-h-screen flex flex-col justify-center items-center gap-2">
+      <h1 className="text-4xl uppercase text-center font-bold">
+        Hello <span className="text-purple-700">Started</span> pack
+      </h1>
+      <h4 className="text-center p-1">
+        <b>Technologies :</b>
+        <ul className="flex gap-2.5 mt-2">
+          {/* Nextjs */}
+          <li>
+            <span className="inline-flex items-center justify-center rounded-6 bg-black font-medium px-2.5 py-0.5 text-gray-100 dark:bg-emerald-700 dark:text-emerald-100">
+              <p className="text-sm whitespace-nowrap">Nextjs 15.3</p>
+            </span>
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          {/* tailwindcss */}
+          <li>
+            <span className="inline-flex items-center justify-center rounded-6 bg-sky-500 font-medium px-2.5 py-0.5 text-gray-50 dark:bg-emerald-700 dark:text-emerald-100">
+              <p className="text-sm whitespace-nowrap">Tailwindcss</p>
+            </span>
           </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          {/* Ant design */}
+          <li>
+            <span className="inline-flex items-center justify-center rounded-6 bg-blue-500 font-medium px-2.5 py-0.5 text-gray-100 dark:bg-emerald-700 dark:text-emerald-100">
+              <p className="text-sm whitespace-nowrap">Ant Design</p>
+            </span>
+          </li>
+        </ul>
+      </h4>
+      <Link href="https://www.github.com/AR-Tausif" className="group">
+        <h4 className="text-center p-1 flex items-center gap-x-3 font-bold ">
+          <p className="bg-gray-300 p-1 rounded-full text-center">
+            <Github />
+          </p>
+          Developed by: Tausif Ahmed
+          <Link2 className="hidden text-gray-600 group-hover:block" />
+        </h4>
+      </Link>
     </div>
   );
 }
